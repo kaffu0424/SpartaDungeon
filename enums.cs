89c0a91beafc8482c16494fe8773ae18843cb904
kaffu <kaffu@naver.com>
2025-01-31 19:20:39 +0900

    싱글톤, 매니저, Enum 추가
    1. 매니저클래스를 위한 싱글톤
    2. Player와 Scene을 관리할 매니저 추가
    3. 플레이어 직업과 씬네임 Enum 작성

---
+++ enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    public enum JobType
+    {
+        Warrior,
+        Rogue
+    }
+
+    public enum SceneName
+    {
+        UserCreateScene,
+        LobbyScene,
+        StatScene,
+        InventoryScene,
+        ShopScene,
+        DungeonScene,
+        SleepScene
+    }
+}
