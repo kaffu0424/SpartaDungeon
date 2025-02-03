88dffb56505bf6238babe0265f7260c022633b1f
kaffu <kaffu@naver.com>
2025-02-03 11:20:29 +0900

    Lobby Scene 완료
    Lobby Scene에서 다른 Scene으로 이동가능

--- Scene/LobbyScene.cs
+++ Scene/LobbyScene.cs
@@ -10,8 +10,44 @@ namespace SpartaDungeon
     {
         public override void Print()
         {
-            Console.WriteLine("Lobby Scene \n >>");
-            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
+
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine("4. 던전입장");
+            Console.WriteLine("5. 휴식하기");
+
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+
+            Console.Write("> ");
+            string command = Console.ReadLine();
+
+            // 입력 확인
+            if (!int.TryParse(command, out int intCommand))
+                return;
+
+            switch(intCommand)
+            {
+                case 1:
+                    SceneManager.Instance.ChangeScene(SceneName.StatScene);
+                    break;
+                case 2:
+                    SceneManager.Instance.ChangeScene(SceneName.InventoryScene);
+                    break;
+                case 3:
+                    SceneManager.Instance.ChangeScene(SceneName.ShopScene);
+                    break;
+                case 4:
+                    SceneManager.Instance.ChangeScene(SceneName.DungeonScene);
+                    break;
+                case 5:
+                    SceneManager.Instance.ChangeScene(SceneName.SleepScene);
+                    break;
+            }
         }
     }
 }
